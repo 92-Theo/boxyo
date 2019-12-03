bbba4edb49a4206dd3fb3847a51566c0e666ebb1
theo <92-theo@naver.com>
2019-12-04 03:24:39 +0900

    Add Globalization

---
+++ front-end/xamarin/boxyo/boxyo/TranslateExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace boxyo
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        const string ResourceId = "boxyo.Resx.AppResources";
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+            ResourceManager resourceManager = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+
+            return resourceManager.GetString(Text, CultureInfo.CurrentCulture);
+        }
+    }
+}
